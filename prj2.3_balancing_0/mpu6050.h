fe740dc5addc60283c791e03fe2d8e1618310c51
Kijong Uhm <kijong.uhm@gmail.com>
2017-10-13 21:30:24 +0900

    IMU uses Kalman filter

--- prj2.3_balancing_0/mpu6050.h
+++ prj2.3_balancing_0/mpu6050.h
@@ -1,22 +1,15 @@
 #ifndef __MPU6050_H__
 #define __MPU6050_H__
 
-#define COMPLEMENTARY_ALPHA		0.98
+#define COMPLEMENTARY_ALPHA		0.93
 
 extern void mpu6050_setup(void);
 extern void mpu6050_loop(void);
-
-extern unsigned long get_last_time();
-extern float get_last_x_angle();
-extern float get_last_y_angle();
-extern float get_last_z_angle();
-extern float get_last_gyro_x_angle();
-extern float get_last_gyro_y_angle();
-extern float get_last_gyro_z_angle();
-
-extern float get_yaw_angle(void);
 extern float get_pitch_angle(void);
-extern float get_roll_angle(void);
+
+extern double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+extern double compAngleX, compAngleY; // Calculated angle using a complementary filter
+extern double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
 
 
 #endif // __MPU6050_H__
