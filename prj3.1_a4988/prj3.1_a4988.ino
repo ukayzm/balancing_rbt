4091ee589184ad063a3417aab2e3a0463927736d
Kijong Uhm <kijong.uhm@gmail.com>
2018-04-15 17:21:51 +0900

    speed control using timer2

--- prj3.1_a4988/prj3.1_a4988.ino
+++ prj3.1_a4988/prj3.1_a4988.ino
@@ -14,7 +14,6 @@
 int stp = 5;  //connect pin 13 to step
 int dir = 6;  // connect pin 12 to dir
 int en = 7;  // connect pin 12 to dir
-int a = 0;     //  gen counter
 
 void setup() 
 {                
@@ -23,33 +22,80 @@ void setup()
 	pinMode(dir, OUTPUT);       
 	pinMode(en, OUTPUT);       
 	digitalWrite(en, LOW);   
+
+	/*
+	 * set timer2 interrupt at 8kHz
+	 * ref: http://www.instructables.com/id/Arduino-Timer-Interrupts
+	 */
+	cli();//stop interrupts
+
+	TCCR2A = 0;// set entire TCCR2A register to 0
+	TCCR2B = 0;// same for TCCR2B
+	TCNT2  = 0;//initialize counter value to 0
+	// set compare match register for 8khz increments
+	OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+	// turn on CTC mode
+	TCCR2A |= (1 << WGM21);
+	// Set CS21 bit for 8 prescaler
+	TCCR2B |= (1 << CS21);   
+	// enable timer compare interrupt
+	TIMSK2 |= (1 << OCIE2A);
+
+	sei();//allow interrupts
+
+	digitalWrite(dir, HIGH);
+}
+
+/*
+ * ISR for timer2 runs at 8kHz frequency.
+ */
+uint16_t cnt, max_cnt;
+ISR(TIMER2_COMPA_vect)
+{
+	if (max_cnt > 0) {
+		if (cnt == 0) {
+			digitalWrite(stp, HIGH);  
+		} else if (cnt == 1) {
+			digitalWrite(stp, LOW);  
+		}
+		cnt++;
+		if (cnt > max_cnt) {
+			cnt = 0;
+		}
+	}
+}
+
+uint16_t rpm2maxcnt(uint16_t rpm)
+{
+	float pps = rpm * 200.0 / 60.0;
+	return (uint16_t)(8000.0 / pps);
+}
+
+void set_rpm(int16_t rpm)
+{
+	if (rpm == 0) {
+		max_cnt = 0;
+		cnt = 0;
+		digitalWrite(stp, LOW);
+	}
+	max_cnt = rpm2maxcnt(rpm);
+	Serial.print(rpm);
+	Serial.print(", ");
+	Serial.println(max_cnt);
 }
 
 void loop() 
 {
-	if (a <  200)  //sweep 200 step in dir 1
-	{
-		a++;
-		digitalWrite(stp, HIGH);   
-		delay(10);               
-		digitalWrite(stp, LOW);  
-		delay(10);              
-		Serial.println(a);
-	}
-	else     //sweep 200 in dir 2
-	{
-		digitalWrite(dir, HIGH);
-		a++;
-		digitalWrite(stp, HIGH);  
-		delay(10);               
-		digitalWrite(stp, LOW);  
-		delay(10);
+	int rpm;
 
-		if (a>400)  // Initialize
-		{
-			a = 0;
-			digitalWrite(dir, LOW);
-		}
-		Serial.println(a);
+	for (rpm = 0; rpm <= 30; rpm += 1)
+	{
+		set_rpm(rpm);
+		delay(1000);
+	}
+	for (rpm = 30; rpm <= 300; rpm += 10)
+	{
+		set_rpm(rpm);
+		delay(1000);
 	}
 }
